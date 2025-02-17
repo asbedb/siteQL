41158e44380eb4cf6d91b405cce41864710244fc
Asbed B <abb401@gmail.com>
2025-02-17 11:11:09 +1100

    removed redundant useState and useEffect, ensured initial theme render matched button state.

--- src/app/components/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher.tsx
@@ -2,19 +2,11 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { Switch } from "@nextui-org/react";
 import { FaSun, FaMoon } from "react-icons/fa6";
 
 export function ThemeSwitcher() {
-    const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) return null;
 
     const handleThemeChange = (isSelected: boolean) => {
         setTheme(isSelected ? "light" : "dark");
